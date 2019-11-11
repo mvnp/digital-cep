4b835a45cbda9f73f4cdc4740ecfc447a85ec614
Marcos Vinicius Nasc Pereira <mvnpereira@gmail.com>
2019-11-11 19:22:36 -0300

    Busca de CEP no ViaCep

---
+++ exemplo.php
@@ -0,0 +1,15 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require "vendor/autoload.php";
+
+use \Marcos\Package\Search;
+
+$busca = new Search;
+
+$resultado = $busca->getAddressFromZipCode("88132600");
+
+echo "<pre>"; print_r($resultado);
